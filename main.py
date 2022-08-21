b39046a672234908aad0b4b09249fa75b78541a8
Mattazz <matthewazada101@gmail.com>
2022-08-21 23:27:47 +0800

    Updated to-do list

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ print('''
 Bang Bang Salary computation with deductions for net pay slip generation
 V0.1
 @Mattazz
-
-Notes: 
-- Salary benefit deductions are computed based on a per day basis. Not sure if this is how it goes in real life.
-ex. (SSS Deduction / (Total Days / Day Range)
-ex. (600 / (30/15)
-ex. (600 / 2)
-ex. SSS Deduction = 300
 ''')
 
 # ID : ['NAME', Wage, Philhealth, SSS, PagIbig]
