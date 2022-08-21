24a4d5fbb67674bf3391aad146698c59cf6cc14c
Mattazz <matthewazada101@gmail.com>
2022-08-22 03:14:18 +0800

    Attempting to implemente "All Output" code but still stuck at how to properly output DF with list of multiple dictionaries

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import openpyxl
 from datetime import date
 from os.path import exists
+import numpy
 
 print('''
  ____        _                   ____                            _
@@ -17,81 +18,145 @@ V0.1
 
 # ID : ['NAME', Wage, Philhealth, SSS, PagIbig]
 employeeDic = {
-    '1': ['Janet Apostol', 533, 277.16, 600, 100],
-    '2': ['Rizalyn Repalda', 533, 277.16, 600, 100]
+    '0': ['Janet Apostol', 533, 277.16, 600, 100],
+    '1': ['Rizalyn Repalda', 533, 277.16, 600, 100]
 }
 print()
 # Input getting data of girls
 print('''Which employee:
-    (1) Janet Apostol
-    (2) Rizalyn Repalda
+    (0) Janet Apostol
+    (1) Rizalyn Repalda
+    (2) All
     ''')
 
 # Input a valid response
 while True:
     try:
         employeeSelect = input('Select (1/2): ')
-        if int(employeeSelect) in range(1, 3):
+        if int(employeeSelect) in range(0, 3):
             break
     except:
         pass
     print('Input a valid number')
 
-# Making it easier to read by changing dictionary values to vars
-inputName = employeeDic[employeeSelect][0]
-inputWageperDay = employeeDic[employeeSelect][1]
-inputPhilhealth = employeeDic[employeeSelect][2]
-inputSSS = employeeDic[employeeSelect][3]
-inputPagibig = employeeDic[employeeSelect][4]
+allDict = []
 
-print()
-print(f'Getting wage info...')
-print('=' * 80)
-print(f'''
-    Name: {inputName}
-    Wage per Day: {inputWageperDay}
-    Phil-health: {inputPhilhealth}
-    SSS: {inputSSS}
-    Pag-Ibig: {inputPagibig}
-''')
-print('=' * 80)
+if employeeSelect == '2':
+    print('_All_ feature not implemented yet')
 
-print()
-wageDays = input('Input the number of days to compute for wage: ')
-wageDays = int(wageDays)
+    for i in range(0,len(employeeDic)):
+        inputName = employeeDic[str(i)][0]
+        inputWageperDay = employeeDic[str(i)][1]
+        inputPhilhealth = employeeDic[str(i)][2]
+        inputSSS = employeeDic[str(i)][3]
+        inputPagibig = employeeDic[str(i)][4]
 
-salaryAmount = int(wageDays) * int(employeeDic[employeeSelect][1])
+        print()
+        print(f'Getting wage info...')
+        print('=' * 80)
+        print(f'''
+                Name: {inputName}
+                Wage per Day: {inputWageperDay}
+                Phil-health: {inputPhilhealth}
+                SSS: {inputSSS}
+                Pag-Ibig: {inputPagibig}
+            ''')
+        print('=' * 80)
 
-# Computing benefit deductions
-sssDeduction = int(employeeDic[employeeSelect][3]) / (30 / wageDays)
-philHealthDeduction = int(employeeDic[employeeSelect][2]) / (30 / wageDays)
-pagIbigDeduction = int(employeeDic[employeeSelect][4]) / (30 / wageDays)
+        print()
+        wageDays = input('Input the number of days to compute for wage: ')
+        wageDays = int(wageDays)
 
-netSalary = salaryAmount - sssDeduction - philHealthDeduction - pagIbigDeduction
+        salaryAmount = int(wageDays) * int(employeeDic[str(i)][1])
 
-print('=' * 80)
-print(f'Salary amount: {salaryAmount}')
-print(f'Philhealth Deduction: {philHealthDeduction}')
-print(f'SSS Deduction: {sssDeduction}')
-print(f'PagIbig Deduction: {pagIbigDeduction}')
-print()
-print(f'Net Salary: {netSalary}')
-print('=' * 80)
+        # Computing benefit deductions
+        sssDeduction = int(employeeDic[str(i)][3]) / (30 / wageDays)
+        philHealthDeduction = int(employeeDic[str(i)][2]) / (30 / wageDays)
+        pagIbigDeduction = int(employeeDic[str(i)][4]) / (30 / wageDays)
 
-# Generate Pandas Dataframe with output
-outputDic = pd.DataFrame({inputName: [salaryAmount, philHealthDeduction, sssDeduction, pagIbigDeduction, netSalary]})
-outputDic.index = ['Gross Salary', 'PhilHealth Deduction', 'SSS Deduction', 'Pag-Ibig Deduction', 'Net Salary']
-print(outputDic)
+        netSalary = salaryAmount - sssDeduction - philHealthDeduction - pagIbigDeduction
 
-print('=' * 80)
+        print('=' * 80)
+        print(f'Salary amount: {salaryAmount}')
+        print(f'Philhealth Deduction: {philHealthDeduction}')
+        print(f'SSS Deduction: {sssDeduction}')
+        print(f'PagIbig Deduction: {pagIbigDeduction}')
+        print()
+        print(f'Net Salary: {netSalary}')
+        print('=' * 80)
 
-print('Please double check if information is accurate.')
+        # Generate Pandas Dataframe with output
+        allOutput = {i: [inputName, salaryAmount, philHealthDeduction, sssDeduction, pagIbigDeduction, netSalary]}
+        allDict.append(allOutput)
+        print(allDict)
+
+        print('=' * 80)
+
+        print('Please double check if information is accurate.')
+else:
+    # Making it easier to read by changing dictionary values to vars
+    inputName = employeeDic[employeeSelect][0]
+    inputWageperDay = employeeDic[employeeSelect][1]
+    inputPhilhealth = employeeDic[employeeSelect][2]
+    inputSSS = employeeDic[employeeSelect][3]
+    inputPagibig = employeeDic[employeeSelect][4]
+
+    print()
+    print(f'Getting wage info...')
+    print('=' * 80)
+    print(f'''
+        Name: {inputName}
+        Wage per Day: {inputWageperDay}
+        Phil-health: {inputPhilhealth}
+        SSS: {inputSSS}
+        Pag-Ibig: {inputPagibig}
+    ''')
+    print('=' * 80)
+
+    print()
+    wageDays = input('Input the number of days to compute for wage: ')
+    wageDays = int(wageDays)
+
+    salaryAmount = int(wageDays) * int(employeeDic[employeeSelect][1])
+
+    # Computing benefit deductions
+    sssDeduction = int(employeeDic[employeeSelect][3]) / (30 / wageDays)
+    philHealthDeduction = int(employeeDic[employeeSelect][2]) / (30 / wageDays)
+    pagIbigDeduction = int(employeeDic[employeeSelect][4]) / (30 / wageDays)
+
+    netSalary = salaryAmount - sssDeduction - philHealthDeduction - pagIbigDeduction
+
+    print('=' * 80)
+    print(f'Salary amount: {salaryAmount}')
+    print(f'Philhealth Deduction: {philHealthDeduction}')
+    print(f'SSS Deduction: {sssDeduction}')
+    print(f'PagIbig Deduction: {pagIbigDeduction}')
+    print()
+    print(f'Net Salary: {netSalary}')
+    print('=' * 80)
+
+    # Generate Pandas Dataframe with output
+    outputDic = pd.DataFrame({'0': [inputName, salaryAmount, philHealthDeduction, sssDeduction, pagIbigDeduction, netSalary]})
+    outputDic.index = ['Name', 'Gross Salary', 'PhilHealth Deduction', 'SSS Deduction', 'Pag-Ibig Deduction', 'Net Salary']
+    print(outputDic)
+
+    print('=' * 80)
+
+    print('Please double check if information is accurate.')
+
+allDictTEST = pd.DataFrame(allDict)
+print(allDictTEST)
+
+# allOutputDict.index = ['Gross Salary', 'PhilHealth Deduction', 'SSS Deduction', 'Pag-Ibig Deduction', 'Net Salary']
 outputQuestion = input('Would you want to save this info? (y/n)')
 
 # Output to excel file
 if outputQuestion == 'y':
     fileName = f'BBSalary_{date.today()}.xlsx'
-    outputDic.to_excel(fileName)
+    if employeeSelect == '2':
+        allDictTEST.to_excel(fileName)
+    else:
+        outputDic.to_excel(fileName)
 else:
-    print("Ending program, thank you!")
+    print("Ending program.")
 
